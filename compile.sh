464aff8bb8c4368230c8fb8111ded8096794f312
Giacomo Serafini <gserafin@tsa-ln002.cm.cluster>
2020-11-22 21:57:04 +0100

    add register spill info

--- compile.sh
+++ compile.sh
@@ -2,7 +2,7 @@
 
 # NEED TO source load-env.sh FIRST!
 echo "nvcc compile"
-nvcc --compile -I./ -o exercise_cuda.o exercise_cuda.cu
+nvcc --compile -I./ -o exercise_cuda.o --ptxas-options=-v exercise_cuda.cu
 echo "g++ compile driver"
 g++ -c -std=c++17 -I./ exercise_driver.cpp utils/atlasToGlobalGpuTriMesh.cpp
 echo "g++ link"
