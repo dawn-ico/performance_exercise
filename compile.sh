64578058267ea3f4aaee1aaac82b052ab9622a86
Giacomo Serafini <gserafin@tsa-ln002.cm.cluster>
2020-11-22 22:24:55 +0100

    Fix spill info display

--- compile.sh
+++ compile.sh
@@ -2,7 +2,7 @@
 
 # NEED TO source load-env.sh FIRST!
 echo "nvcc compile"
-nvcc --compile -I./ -o exercise_cuda.o --ptxas-options=-v exercise_cuda.cu
+( nvcc --compile -I./ -o exercise_cuda.o --ptxas-options=-v exercise_cuda.cu 3>&1 1>&2 2>&3 | grep "spill" ) 3>&1 1>&2 2>&3
 echo "g++ compile driver"
 g++ -c -std=c++17 -I./ exercise_driver.cpp utils/atlasToGlobalGpuTriMesh.cpp
 echo "g++ link"
